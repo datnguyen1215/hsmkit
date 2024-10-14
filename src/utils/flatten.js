d3cc1d6ee253453102be1b6e2ea6a114d8f2226b
Dat Nguyen <datnguyen1215@gmail.com>
2024-10-13 18:32:44 -0700

    add error handling/jsdoc for flatten().

--- src/utils/flatten.js
+++ src/utils/flatten.js
@@ -1,4 +1,13 @@
+import assert from './assert';
+
+/**
+ * Flatten an array of nested arrays.
+ * @param {Array} arr - The array to flatten.
+ * @returns {Array} The flattened array.
+ **/
 const flatten = arr => {
+  assert(Array.isArray(arr), 'flatten expects an array');
+
   return arr.reduce(
     (acc, val) =>
       Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val),
