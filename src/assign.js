470877a17598ce8ab6250bb72801ef62780b3710
Dat Nguyen <datnguyen1215@gmail.com>
2024-10-13 18:44:48 -0700

    use merge for assign().

--- src/assign.js
+++ src/assign.js
@@ -1,15 +1,25 @@
 import assert from './utils/assert';
+import merge from './utils/merge';
 
 /**
- * @param {object} obj
+ * Creates an assign action that updates the context of the state machine.
+ *
+ * @param {object} obj - The object containing keys and values to assign to the context.
+ *                        Values can be functions that take the context and event as parameters.
+ * @returns {function} - A function that takes the current context and event, then updates the context.
  */
 const assign = obj => (context, event) => {
   assert(obj, 'obj is required');
 
-  Object.entries(obj).forEach(([key, value]) => {
-    if (typeof value === 'function') context[key] = value(context, event);
-    else context[key] = value;
-  });
+  const values = Object.entries(obj).reduce(
+    (acc, [key, value]) => ({
+      ...acc,
+      [key]: typeof value === 'function' ? value(context, event) : value
+    }),
+    {}
+  );
+
+  merge(context, values);
 };
 
 export default assign;
